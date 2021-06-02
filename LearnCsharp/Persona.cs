c5b8538589c7de3287438940632673e218b5c3c4
htorrico <htorrico@tecsup.edu.pe>
2021-06-01 21:04:54 -0500

    Agregar archivos de proyecto.

---
+++ LearnCsharp/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCsharp
+{
+    /// <summary>
+    /// Declaración de clases
+    /// </summary>
+    public class Persona
+    {
+        public int PersonaId { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+
+        //Atajo 1: para propiedades
+        // Escribir prop y presionar tab 2 veces
+
+        public int Edad { get; set; }
+
+    }
+}
---
+++ LearnCsharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Declarar variables
+            int edad = 0;
+            string nombre = "";
+            bool EsPeruano = false;
+            double sueldo = 8000;
+
+
+
+
+            Console.WriteLine("Hello world");
+            Console.Read();
+        }
+    }
+}
